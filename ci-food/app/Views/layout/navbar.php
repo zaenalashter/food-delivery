be2598555d9ee9b31bb82ced061067e567e08858
zaenalashter <santrialit77@gmail.com>
2022-04-17 22:44:32 +0700

    Mengatur Tampilan Admin

---
+++ ci-food/app/Views/layout/navbar.php
@@ -0,0 +1,42 @@
+<header class="main-header">
+    <a href="<?= site_url('dashboard') ?>" class="logo">
+        <span class="logo-mini"><b>F</b>D</span>
+        <span class="logo-lg"><b>Food</b>Delivery</span>
+    </a>
+
+    <nav class="navbar navbar-static-top">
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <img src="<?= base_url(); ?>/images/user.png" class="user-image" alt="User Image">
+                        <span class="hidden-xs">
+                            Zaenalashter 
+                            <i class="fa fa-caret-down" style="margin-left: 5px"></i>
+                        </span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="user-header">
+                            <img src="<?= base_url(); ?>/images/user.png" class="img-circle" alt="User Image">
+                            <p>
+                                Zaenalashter
+                                <small>email : hoho@gmail.com</small>
+                            </p>
+                        </li>
+                        <li class="user-footer">
+                            <div class="pull-left">
+                                <a href="#" class="btn btn-default btn-flat">Profile</a>
+                            </div>
+                            <div class="pull-right">
+                                <a href="javascript:void(0)" class="btn btn-default btn-flat">Sign out</a>
+                            </div>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
